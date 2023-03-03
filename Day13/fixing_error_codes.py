8881a4a1227cc559dcc21a8ec66f2dc1cfed4160
SQA-SOLOMAN <kavkazus@hotmail.com>
2023-03-03 16:57:57 -0500

    Day13 is Done

---
+++ Day13/fixing_error_codes.py
@@ -0,0 +1,42 @@
+# number = int(input("Which number do you want to check?"))
+#
+# if number % 2 = 0:
+#   print("This is an even number.")
+# else:
+#   print("This is an odd number.")
+#
+
+
+number = int(input("Which number do you want to check?"))
+
+if number % 2 == 0:
+    print("This is an even number.")
+else:
+    print("This is an odd number.")
+
+# year = input("Which year do you want to check?")
+#
+# if year % 4 == 0:
+#   if year % 100 == 0:
+#     if year % 400 == 0:
+#       print("Leap year.")
+#     else:
+#       print("Not leap year.")
+#   else:
+#     print("Leap year.")
+# else:
+#   print("Not leap year.")
+#
+
+year = int(input("Which year do you want to check?"))
+
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print("Leap year.")
+        else:
+            print("Not leap year.")
+    else:
+        print("Leap year.")
+else:
+    print("Not leap year.")
